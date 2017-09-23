2ed9b3ad3524efc2776b0592ae03047a3c187e01
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2017-09-23 13:46:43 +0530

    testing on slimerjs with highloading time website

---
+++ slimerjs/iatdg/Readme.md
@@ -0,0 +1,8 @@
+install casperjs
+npm install -g casperjs
+
+Install slimerjs
+npm install -g slimerjs
+
+
+casperjs --verbose --engine=slimerjs test tests --include=function.js
---
+++ slimerjs/iatdg/tests/test1.js
@@ -0,0 +1,34 @@
+casper.test.begin('Testing IATDG', 5, function(test) {
+    casper.options.viewportSize = { width: 1280, height: 800 };
+    
+    casper.start('http://iatdg.org/', function() {
+      this.echo('In home page');
+      test.assertVisible('body');
+      test.assertSelectorHasText('#menu-item-16 > a', 'Home');
+      
+    });
+  
+    casper.then(function() {
+      test.assertTitle('IATDG');
+      casper.capture('home.png');
+      
+    });
+
+    casper.then(function(){
+      casper.waitTimeout=60000;
+      this.echo('clicking about page');
+      this.click('#menu-item-17 > a');
+      casper.wait(25000, function() {
+        this.echo('20');
+   });
+      this.echo('about page opened succesfully');
+      test.assertVisible('body');
+      test.assertTitle('IATDG');
+      casper.capture('about.png');
+    });
+
+  
+    casper.run(function() {
+      test.done();
+    });
+  });
